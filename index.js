1a495386f3426037baef18102cd9a05daee208fe
fatpat314 <w.patrick.kelly@gmail.com>
2020-12-08 16:03:36 -0800

    indent

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ module.exports = {
   sayHi: () => {
       const { exec } = require("child_process");
 
-exec("open https://www.youtube.com/watch?v=5qap5aO4i9A", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`yo`);
-});
+        exec("open https://www.youtube.com/watch?v=5qap5aO4i9A", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`yo`);
+        });
   }
 }
 
